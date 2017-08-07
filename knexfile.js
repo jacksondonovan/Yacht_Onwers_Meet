340a0da3ffd0f1a196147d3f6cc0882e5b1aa50c
Jackson Donovan <iinventedtwitter@gmail.com>
2017-08-07 12:57:20 -0600

    fixed snytax error

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,5 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: 'postgres://jyafkxqkqguzze:25724566de7a6191179d71197f378fe736f4584dd35f93e38a66cc38f2e82c46@ec2-50-19-105-113.compute-1.amazonaws.com:5432/d4abv9i3sgth9a'
-};
+  }
+}
